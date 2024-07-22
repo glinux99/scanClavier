214a9a73293c609879fd0f105d73f1054b1f1deb
Linux99 <genesiskikimba@gmail.com>
2024-07-22 22:20:22 +0200

    adding

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -23,71 +23,70 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  int count =0;
-  List <Map<String, dynamic>> liste =[{}];
+  int count = 0;
+  Map<String, dynamic> scan = {};
+  List<Map<String, dynamic>> liste = [{}];
   String result = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("VE COMPTEUR SCAN", style: TextStyle(color: Colors.white)),backgroundColor: Color.fromARGB(221, 238, 133, 5),),
-      body: Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Barcode: $result'),
-          const Text("-----------------------------------------------------"),
-          ListView.builder(itemBuilder: (context, i){
-            Map _data = liste[i];
-            return ListTile(
-              leading: Icon(Icons.keyboard),
-              title: Text("Compteur ${_data['compteur']}"),
-              subtitle: Text("Clavier ${_data['clavier']}"),
-            );
-          })
-        ],
+      appBar: AppBar(
+        title: const Text("VE COMPTEUR SCAN",
+            style: TextStyle(color: Colors.white)),
+        backgroundColor: const Color.fromARGB(221, 238, 133, 5),
       ),
-    )
-,
-        floatingActionButton: FloatingActionButton(
+      body: Center(
+        child: ListView.builder(
+          itemCount: liste.length,
+          itemBuilder: (context, i) {
+            Map data = liste[i];
+            return ListTile(
+              leading: const Icon(Icons.keyboard),
+              title: Text("Compteur ${data['compteur']}"),
+              subtitle: Text("Clavier ${data['clavier']}"),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
         onPressed: () async {
-                var res = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SimpleBarcodeScannerPage(),
-                    ));
-                setState(() {
-                  if (res is String) {
-                    result = res;
-                    count ++;
-                    if(result!='-1'){
-                      Map <String, dynamic> scan ={
-                        
-                      };
-                      if(count ==1){
-                       scan[ "clavier"]= result;
-                      }
-                      if(count==2){
-                        scan['compteur'] = result;
-                        liste.add(scan);
-                      }
-                      // liste.add(result);
-                    }else{
-                      
-                    }
-                  }
-                });
-              },
+          var res = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SimpleBarcodeScannerPage(),
+              ));
+          setState(() {
+            if (res is String) {
+              result = res;
+              count++;
+              print(count);
+
+              if (result != '-1') {
+                if (count == 1) {
+                  scan["clavier"] = result;
+                }
+                if (count == 2) {
+                  scan['compteur'] = result;
+                  liste.add(scan);
+                  print(scan);
+                  count = 0;
+                  scan = {};
+                }
+                // liouste.add(result);
+              } else {}
+            }
+          });
+        },
         tooltip: 'add',
         child: const Icon(Icons.qr_code_2),
-      ), 
+      ),
     );
   }
 }
